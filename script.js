e20917fcc5d214260501fa115a37696a969c3da6
Pete Fowler <fowler.pete@gmail.com>
2022-07-24 21:26:33 -0600

    Add JS for nav btn functionality

---
+++ script.js
@@ -0,0 +1,20 @@
+const topBtn = document.querySelector('#top-btn');
+topBtn.addEventListener('click', () => {
+  document.getElementById("main").scrollIntoView({behavior: 'smooth'})});
+
+const projectsBtn = document.querySelector('#projects-btn');
+projectsBtn.addEventListener('click', () => {
+  document.getElementById("projects").scrollIntoView({behavior: 'smooth'})});
+
+const bioBtn = document.querySelector('#bio-btn');
+bioBtn.addEventListener('click', () => {
+  document.getElementById("bio").scrollIntoView({behavior: 'smooth', block: 'center'})});
+
+const linksBtn = document.querySelector('#links-btn');
+linksBtn.addEventListener('click', () => {
+  document.getElementById("links").scrollIntoView({behavior: 'smooth', block: 'center'})});
+
+const contactBtn = document.querySelector('#contact-btn');
+contactBtn.addEventListener('click', () => {
+  document.getElementById("contact").scrollIntoView({behavior: 'smooth', block: 'center'})});
+
